3a4ed3e1e1ce124ebf007bc8b535a72480b703b6
Arthur-kin <arthur940506@gmail.com>
2026-03-13 10:33:38 +0800

    add insertionsort note

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -2,16 +2,17 @@
 #include <vector>
 
 //Insertion_sort function
+//從第二位開始對照左邊的值的大小 大的話就交換
 void insertion_sort(std::vector<int>&A){
     int n = A.size();
     for (int i= 1;i < n;++i){
         int key = A[i];
         int j = i-1;
         while(j >= 0 && A[j] > key){
-            A[j+1] = A[j];
-            --j;
+            A[j+1] = A[j];//交換
+            --j;//再往左對照
         }
-        A[j+1] = key;
+        A[j+1] = key;//都換完之後 把自己放進去
     }
 }
 
