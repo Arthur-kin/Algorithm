eb904ad1c6339a5d3912c73e77d3a8209d16674b
Arthur-kin <arthur940506@gmail.com>
2026-03-19 00:18:36 +0800

    finish heap_sort

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -9,13 +9,14 @@ void bubble_sort(vector<int>&A){
     for (int i = 0;i < n-1;++i){
         for (int j = n-1;j >= i+1;--j){
           if (A[j] < A[j-1]){
-            swap(A[j], A[j-1]);
+            swap(A[j], A[j-1]);//新東西 但要加上面的algorithm
           }
         }      
     }
 }
 
 int main(){
+    
     vector<int> A = {4, 3, 9, 1};
     cout << "排序前: ";
     for (int num : A) { //新用法
--- heap_sort.cpp
+++ heap_sort.cpp
@@ -3,4 +3,58 @@
 
 using namespace std;
 
-void build
+void max_heaplfy(vector<int>&A ,int i, int A_heap_size){
+    
+    int l = 2*i+1;
+    int r = 2*i+2;
+
+    int largest = i;
+    
+    if (l < A_heap_size && A[l] > A[largest]){
+        largest = l;
+    }else{
+        largest = i;
+    }
+    if (r < A_heap_size && A[r] > A[largest]){
+        largest = r;
+    }
+    if (largest != i){
+        swap(A[i], A[largest]);
+        max_heaplfy(A, largest , A_heap_size);
+    }
+
+
+}
+
+void build_max_heap(vector<int>&A, int A_heap_size){
+
+    for (int i =(A_heap_size)/2;i > -1;--i){
+        max_heaplfy(A,i,A_heap_size);
+    }
+
+}
+void heap_sort(vector<int>&A){
+    int A_heap_size = A.size();
+    build_max_heap(A, A_heap_size);//先把A調整成max heap
+    for (int i = 0; i < A.size(); ++i){
+        swap(A[0], A[A.size()-1-i]);//把最大的放到最後面
+        A_heap_size = A_heap_size - 1;//把heap size減一
+        max_heaplfy(A, 0, A_heap_size);//重新調整成max heap  
+    }
+    
+
+}
+
+int main(){
+    vector<int> A = {4 ,3 ,1 ,9};
+    cout<<"排序前:";
+    for (int num : A){
+        cout<< num << " ";
+    }
+    heap_sort(A);
+    cout << "\n";
+    cout<<"排序後:";
+    for (int num : A){
+        cout<< num << " ";
+    }
+}
