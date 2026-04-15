deac260d1e8b941f565aa9ece859468b638b6e66
Arthur-kin <arthur940506@gmail.com>
2026-04-16 00:38:24 +0800

    unstable to stable

--- counting_sort.cpp
+++ counting_sort.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 void counting_sort(vector<int>& A ){
-    int max_number = *max_element(A.begin(),A.end());//找到A裡面最大的數字 因為count陣列的大小要是最大數字+1
+    int max_number = *max_element(A.begin(),A.end());//找到A裡面最大的數字 使用＃include<algorithm>裡面的max_element函數 因為count陣列的大小要是最大數字+1
     vector<int> count(max_number+1,0);//建立一個count陣列 來記錄0->max_number數字出現的次數
     
     for (int num : A){
@@ -13,14 +13,15 @@ void counting_sort(vector<int>& A ){
     }
     
     for(int i = 1; i < count.size();++i){
-        count[i] = count[i] + count[i-1];//把count陣列改成前綴和陣列 這樣就可以知道每個數字應該放在output陣列的哪裡
+        count[i] = count[i] + count[i-1];
     }
     
-    vector<int> B(A.size(),0);//建立一個output陣列 來存放排序後的結果
+    vector<int> B(A.size(),0);//建立一個output陣列 
     
-    for (int num : A){
-        B[count[num]-1] = num;
-        --count[num];//把count陣列裡面對應的數字減一 因為已經放了一個了
+    //這裡很重要的是要從後往前面走 才是stable 不然會變成unstable 所以這裡不能用for (int num : A) 因為這樣是從前往後走
+    for (int i = A.size()-1; i >= 0; --i){ //
+        B[count[A[i]]-1] = A[i];
+        --count[A[i]];//把count陣列裡面對應的數字減一 因為已經放了一個了
     }
     A = B;
 }
