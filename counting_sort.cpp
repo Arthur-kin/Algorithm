6d1be2c82e1c00c26c3f44a8ea3c7647617942aa
Arthur-kin <arthur940506@gmail.com>
2026-04-15 23:42:47 +0800

    finished counting_sort

--- counting_sort.cpp
+++ counting_sort.cpp
@@ -1,2 +1,46 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
+
+using namespace std;
+
+void counting_sort(vector<int>& A ){
+    int max_number = *max_element(A.begin(),A.end());//找到A裡面最大的數字 因為count陣列的大小要是最大數字+1
+    vector<int> count(max_number+1,0);//建立一個count陣列 來記錄0->max_number數字出現的次數
+    
+    for (int num : A){
+        ++count[num];
+    }
+    
+    for(int i = 1; i < count.size();++i){
+        count[i] = count[i] + count[i-1];//把count陣列改成前綴和陣列 這樣就可以知道每個數字應該放在output陣列的哪裡
+    }
+    
+    vector<int> B(A.size(),0);//建立一個output陣列 來存放排序後的結果
+    
+    for (int num : A){
+        B[count[num]-1] = num;
+        --count[num];//把count陣列裡面對應的數字減一 因為已經放了一個了
+    }
+    A = B;
+}
+
+int main(){
+    vector<int> A = {4, 3, 9, 1, 11};
+    cout << "排序前: ";
+    for (int num : A) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    counting_sort(A);
+
+    cout << "排序後: ";
+    for (int num : A) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+
+}
